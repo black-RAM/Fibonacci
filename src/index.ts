4111bb66a0f984805b6b381fb75a3326a527fb8d
black-RAM <pplanes311@gmail.com>
2023-12-28 15:35:27 +0300

    optimised recursive fibonacci-sequence generating function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
 import "./style.css";
+
+// memoization to optimize fibonacci sequence generation
+interface Memo {
+  [key: number]: number[]
+}
+
+function fibonacci(count: number, memo: Memo = {0: [], 1: [0], 2: [0, 1]}): number[] {
+  if (memo[count]) {
+    return memo[count]
+  }
+
+  let fibonacciArray = fibonacci(count - 1, memo)
+  
+  fibonacciArray.push(
+    fibonacciArray[fibonacciArray.length - 1] +
+    fibonacciArray[fibonacciArray.length - 2]
+  )
+
+  memo[count] = fibonacciArray;
+  
+  return fibonacciArray;
+}
+
+const fibonacciSequence = fibonacci(20000)
+console.log(fibonacciSequence)
